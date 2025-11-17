6e57885857506e4cf3e9637888703a6b0f71afc9
Daniel <danielmesquitadev@gmail.com>
2025-11-16 22:21:27 -0300

    create project

---
+++ backend/src/test/java/dev/danielmesquita/dmcatalog/DmcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielmesquita.dmcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
