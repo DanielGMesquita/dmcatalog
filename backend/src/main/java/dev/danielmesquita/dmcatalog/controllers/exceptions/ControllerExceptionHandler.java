49a3dbe00b9b85c987e68544efd055f55e692e74
Daniel <danielmesquitadev@gmail.com>
2025-11-30 21:31:52 -0300

    feat: implements controller exception handler

---
+++ backend/src/main/java/dev/danielmesquita/dmcatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,38 @@
+package dev.danielmesquita.dmcatalog.controllers.exceptions;
+
+import dev.danielmesquita.dmcatalog.services.exceptions.DatabaseException;
+import dev.danielmesquita.dmcatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+    HttpStatus status = HttpStatus.NOT_FOUND;
+    StandardError err = new StandardError();
+    err.setTimestamp(Instant.now());
+    err.setStatus(status.value());
+    err.setError("Resource not found");
+    err.setMessage(e.getMessage());
+    err.setPath(request.getRequestURI());
+    return ResponseEntity.status(status).body(err);
+  }
+
+  @ExceptionHandler(DatabaseException.class)
+  public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+    StandardError err = new StandardError();
+    err.setTimestamp(Instant.now());
+    err.setStatus(status.value());
+    err.setError("Database exception");
+    err.setMessage(e.getMessage());
+    err.setPath(request.getRequestURI());
+    return ResponseEntity.status(status).body(err);
+  }
+}
