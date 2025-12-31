fcb3ef33959fab2062ba15f0da2b1456b1f89daa
Daniel <danielmesquitadev@gmail.com>
2025-12-31 17:31:52 -0300

    test: fix test for id validation

--- backend/src/test/java/dev/danielmesquita/dmcatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/dev/danielmesquita/dmcatalog/repositories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package dev.danielmesquita.dmcatalog.repositories;
 import dev.danielmesquita.dmcatalog.entities.Product;
 import dev.danielmesquita.dmcatalog.utils.Factory;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,27 +16,23 @@ public class ProductRepositoryTests {
 
   private long existingId;
   private long nonExistingId;
+  private long countTotalProducts;
 
   @BeforeEach
   public void setUp() {
     // Initialize test data if necessary
     existingId = 1L; // Assume this ID exists in the test database
     nonExistingId = 1000L; // Assume this ID does not exist
+    countTotalProducts = 25L; // Assume there are 25 products in total
   }
 
   @Test
   public void saveShouldPersistWithAutoIncrementWhenIdIsNotNull() {
-    Product product = repository.save(Factory.createProduct());
-    assert (product.getId() != null);
-  }
-
-  @Test
-  public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
-    Product product = repository.save(Factory.createProduct());
+    Product product = Factory.createProduct();
     product.setId(null);
-
     product = repository.save(product);
-    assert (product.getId() == null);
+    Assertions.assertNotNull(product);
+    Assertions.assertEquals(countTotalProducts + 1, product.getId());
   }
 
   @Test
