f381f18b50de61d1b79bfe81e431f35bbdd3c0d3
Daniel <danielmesquitadev@gmail.com>
2026-01-04 15:16:57 -0300

    test: add more tests in  ProductServiceTests

--- backend/src/test/java/dev/danielmesquita/dmcatalog/services/ProductServiceTests.java
+++ backend/src/test/java/dev/danielmesquita/dmcatalog/services/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package dev.danielmesquita.dmcatalog.services;
 
 import dev.danielmesquita.dmcatalog.repositories.ProductRepository;
+import dev.danielmesquita.dmcatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,7 +21,7 @@ public class ProductServiceTests {
   private long existingId;
 
   @BeforeEach
-  public void setUp() throws Exception {
+  public void setUp() {
     existingId = 1L;
   }
 
@@ -28,6 +29,17 @@ public class ProductServiceTests {
   public void deleteShouldDoNothingWhenIdExists() {
     Mockito.when(repository.existsById(existingId)).thenReturn(true);
     Assertions.assertDoesNotThrow(
+        () -> {
+          service.delete(existingId);
+        });
+    Mockito.verify(repository).existsById(existingId);
+  }
+
+  @Test
+  public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+    Mockito.when(repository.existsById(existingId)).thenReturn(false);
+    Assertions.assertThrows(
+        ResourceNotFoundException.class,
         () -> {
           service.delete(existingId);
         });
