0c56b93387a6b5f0f9d707128a582983db5a4eee
Daniel <danielmesquitadev@gmail.com>
2026-02-25 14:19:15 -0300

    feat: implement User and Role

---
+++ backend/src/main/java/dev/danielmesquita/dmcatalog/entities/Role.java
@@ -0,0 +1,51 @@
+package dev.danielmesquita.dmcatalog.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+  private String authority;
+
+  public Role() {}
+
+  public Role(Long id, String authority) {
+    this.id = id;
+    this.authority = authority;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getAuthority() {
+    return authority;
+  }
+
+  public void setAuthority(String authority) {
+    this.authority = authority;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof Role role)) return false;
+    return Objects.equals(id, role.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
+}
