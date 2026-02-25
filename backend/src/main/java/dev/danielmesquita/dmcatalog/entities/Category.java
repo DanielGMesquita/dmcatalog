f6b8f3cfe0c425e43a5c5f00f3dcfa39c29c16a2
Daniel <danielmesquitadev@gmail.com>
2026-02-25 14:18:56 -0300

    fix: set of products for Category

--- backend/src/main/java/dev/danielmesquita/dmcatalog/entities/Category.java
+++ backend/src/main/java/dev/danielmesquita/dmcatalog/entities/Category.java
@@ -2,7 +2,9 @@ package dev.danielmesquita.dmcatalog.entities;
 
 import jakarta.persistence.*;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -20,6 +22,9 @@ public class Category {
 
   @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant updatedAt;
+
+  @ManyToMany(mappedBy = "categories")
+  private Set<Product> products = new HashSet<>();
 
   public Category() {}
 
@@ -58,6 +63,10 @@ public class Category {
 
   public void setUpdatedAt(Instant updatedAt) {
     this.updatedAt = updatedAt;
+  }
+
+  public Set<Product> getProducts() {
+    return products;
   }
 
   @Override
