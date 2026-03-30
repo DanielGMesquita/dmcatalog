5cc5083fa4bf9f17d0d2367e1d867b08dc84cd09
Daniel <danielmesquitadev@gmail.com>
2026-03-29 21:35:53 -0300

    feat: searchAll paged with parameters using SQL query

---
+++ backend/src/main/java/dev/danielmesquita/dmcatalog/projections/ProductProjection.java
@@ -0,0 +1,7 @@
+package dev.danielmesquita.dmcatalog.projections;
+
+public interface ProductProjection {
+  Long getId();
+
+  String getName();
+}
--- backend/src/main/java/dev/danielmesquita/dmcatalog/repositories/ProductRepository.java
+++ backend/src/main/java/dev/danielmesquita/dmcatalog/repositories/ProductRepository.java
@@ -1,16 +1,35 @@
 package dev.danielmesquita.dmcatalog.repositories;
 
 import dev.danielmesquita.dmcatalog.entities.Product;
+import dev.danielmesquita.dmcatalog.projections.ProductProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-  @Query(value = "SELECT obj FROM Product obj JOIN FETCH obj.categories",
-          countQuery = "SELECT COUNT(obj) FROM Product obj JOIN obj.categories")
-  Page<Product> findAllWithCategory(Pageable pageable);
+  @Query(nativeQuery = true,
+          value = """
+                  SELECT DISTINCT tb_product.id, tb_product.name
+                  FROM tb_product
+                  INNER JOIN tb_product_category ON tb_product.id = tb_product_category.product_id
+                  WHERE (:categoriesIds IS NULL OR tb_product_category.category_id IN :categoriesIds)
+                  AND (LOWER(tb_product.name) LIKE LOWER(CONCAT('%', :name, '%')) OR :name IS NULL)
+                  ORDER BY tb_product.name
+                  """,
+          countQuery = """
+                  SELECT COUNT(*) FROM(
+                  SELECT DISTINCT tb_product.id, tb_product.name
+                  FROM tb_product
+                  INNER JOIN tb_product_category ON tb_product.id = tb_product_category.product_id
+                  WHERE (:categoriesIds IS NULL OR tb_product_category.category_id IN :categoriesIds)
+                  AND (LOWER(tb_product.name) LIKE LOWER(CONCAT('%', :name, '%')) OR :name IS NULL)
+                  ORDER BY tb_product.name) AS tb_result
+                  """)
+  Page<ProductProjection> searchAll(List<Long> categoriesIds, String name, Pageable pageable);
 }
