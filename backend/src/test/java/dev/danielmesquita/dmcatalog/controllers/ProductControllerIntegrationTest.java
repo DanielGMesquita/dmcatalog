a00db082d22cfabd9961a77caf40bcbdec005be3
Daniel <danielmesquitadev@gmail.com>
2026-04-05 00:10:24 -0300

    test: test categoryId and name as request param

--- backend/src/test/java/dev/danielmesquita/dmcatalog/controllers/ProductControllerIntegrationTest.java
+++ backend/src/test/java/dev/danielmesquita/dmcatalog/controllers/ProductControllerIntegrationTest.java
@@ -1,5 +1,11 @@
 package dev.danielmesquita.dmcatalog.controllers;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.danielmesquita.dmcatalog.dto.ProductDTO;
 import dev.danielmesquita.dmcatalog.utils.Factory;
@@ -14,22 +20,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 public class ProductControllerIntegrationTest {
 
-  @Autowired
-  private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-  @Autowired
-  private ObjectMapper objectMapper;
+  @Autowired private ObjectMapper objectMapper;
 
   private Long existingId;
   private Long nonExistingId;
@@ -44,23 +42,50 @@ public class ProductControllerIntegrationTest {
 
   @Test
   @WithMockUser
-  public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
-    ResultActions resultActions = mockMvc.perform(get("/products?page0&size=12&sort=name,asc")
-            .accept(MediaType.APPLICATION_JSON));
+  public void findAllShouldReturnSortedPageOrderedByName() throws Exception {
+    ResultActions resultActions =
+        mockMvc.perform(get("/products?page0&size=12").accept(MediaType.APPLICATION_JSON));
 
     resultActions.andExpect(status().isOk());
     resultActions.andExpect(jsonPath("$.content").exists());
     resultActions.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
     resultActions.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
     resultActions.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
-    resultActions.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+    resultActions.andExpect(jsonPath("$.page.totalElements").value(countTotalProducts));
+  }
+
+  @Test
+  @WithMockUser
+  public void findAllShouldReturnSortedPageOrderedByNameWhenNameHasValue() throws Exception {
+    ResultActions resultActions =
+        mockMvc.perform(get("/products?page0&size=12&name=mac").accept(MediaType.APPLICATION_JSON));
+
+    resultActions.andExpect(status().isOk());
+    resultActions.andExpect(jsonPath("$.content").exists());
+    resultActions.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+    resultActions.andExpect(jsonPath("$.page.totalElements").value(1L));
+  }
+
+  @Test
+  @WithMockUser
+  public void findAllShouldReturnSortedPageOrderedByNameWhenCategoryIdHasValue() throws Exception {
+    ResultActions resultActions =
+        mockMvc.perform(
+            get("/products?page0&size=12&categoryId=1,3").accept(MediaType.APPLICATION_JSON));
+
+    resultActions.andExpect(status().isOk());
+    resultActions.andExpect(jsonPath("$.content").exists());
+    resultActions.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+    resultActions.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+    resultActions.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+    resultActions.andExpect(jsonPath("$.page.totalElements").value(countTotalProducts - 2L));
   }
 
   @Test
   @WithMockUser
   public void findByIdShouldReturnProductWhenIdExists() throws Exception {
-    ResultActions resultActions = mockMvc.perform(get("/products/{id}", existingId)
-            .accept(MediaType.APPLICATION_JSON));
+    ResultActions resultActions =
+        mockMvc.perform(get("/products/{id}", existingId).accept(MediaType.APPLICATION_JSON));
 
     resultActions.andExpect(status().isOk());
     resultActions.andExpect(jsonPath("$.id").value(existingId));
@@ -73,11 +98,13 @@ public class ProductControllerIntegrationTest {
     ProductDTO productDTO = Factory.createProductDTO();
     String jsonBody = objectMapper.writeValueAsString(productDTO);
 
-    ResultActions resultActions = mockMvc.perform(put("/products/{id}", existingId)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(jsonBody)
-            .accept(MediaType.APPLICATION_JSON)
-            .with(csrf()));
+    ResultActions resultActions =
+        mockMvc.perform(
+            put("/products/{id}", existingId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody)
+                .accept(MediaType.APPLICATION_JSON)
+                .with(csrf()));
 
     resultActions.andExpect(status().isOk());
     resultActions.andExpect(jsonPath("$.id").value(existingId));
@@ -90,11 +117,13 @@ public class ProductControllerIntegrationTest {
     ProductDTO productDTO = Factory.createProductDTO();
     String jsonBody = objectMapper.writeValueAsString(productDTO);
 
-    ResultActions resultActions = mockMvc.perform(put("/products/{id}", nonExistingId)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(jsonBody)
-            .accept(MediaType.APPLICATION_JSON)
-            .with(csrf()));
+    ResultActions resultActions =
+        mockMvc.perform(
+            put("/products/{id}", nonExistingId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody)
+                .accept(MediaType.APPLICATION_JSON)
+                .with(csrf()));
 
     resultActions.andExpect(status().isNotFound());
   }
