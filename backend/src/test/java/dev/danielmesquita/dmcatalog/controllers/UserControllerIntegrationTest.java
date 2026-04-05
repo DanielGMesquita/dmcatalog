715e496914c18f681a570a137d92a2bfd18e6909
Daniel <danielmesquitadev@gmail.com>
2026-04-05 00:11:54 -0300

    test: correct result actions parsing response

--- backend/src/test/java/dev/danielmesquita/dmcatalog/controllers/UserControllerIntegrationTest.java
+++ backend/src/test/java/dev/danielmesquita/dmcatalog/controllers/UserControllerIntegrationTest.java
@@ -1,5 +1,11 @@
 package dev.danielmesquita.dmcatalog.controllers;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.danielmesquita.dmcatalog.dto.UserDTO;
 import dev.danielmesquita.dmcatalog.entities.User;
@@ -14,12 +20,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -58,7 +58,7 @@ public class UserControllerIntegrationTest {
     resultActions.andExpect(jsonPath("$.content").exists());
     resultActions.andExpect(jsonPath("$.content[0].firstName").value("Alex"));
     resultActions.andExpect(jsonPath("$.content[1].firstName").value("Maria"));
-    resultActions.andExpect(jsonPath("$.totalElements").value(countTotalUsers));
+    resultActions.andExpect(jsonPath("$.page.totalElements").value(countTotalUsers));
   }
 
   @Test
